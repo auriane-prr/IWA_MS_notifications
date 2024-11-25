0ee96144150b0fd388e32959e7c4fb42fc9d6142
MargauxThirion <thirion.margaux@gmail.com>
2024-11-25 14:54:24 +0100

    test

--- src/test/java/org/example/iwa_ms_notification/NotificationControllerTest.java
+++ src/test/java/org/example/iwa_ms_notification/NotificationControllerTest.java
@@ -6,7 +6,6 @@ import org.example.iwa_ms_notification.models.Notification;
 import org.example.iwa_ms_notification.services.NotificationService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,10 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
